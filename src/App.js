49e0d02081480155e9037325c9ea8c185757ef58
Eric <eric248550@gmail.com>
2022-10-26 16:24:11 +0800

    mint

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ export default function App() {
     });
     const [type, setType] = useState();
     const [receivingAddress, setReceivingAddress] = useState();
-    const [ada, setADA] = useState();
+    const [ada, setAda] = useState();
     const [metadata, setMetadata] = useState();
 
 
@@ -50,8 +50,8 @@ export default function App() {
         const params = new URLSearchParams(window.location.search);
         setType(params.get('type'));
         setReceivingAddress(params.get('address'))
-        setADA(params.get('ada'));
-        // setADA(params.get('metadata'));
+        setAda(params.get('ada'));
+        setMetadata(params.get('metadata'));
     })
 
     async function t(walletName) {
@@ -62,8 +62,8 @@ export default function App() {
             window.cardano[walletName],
             // blockfrostApiKey
             {
-            0: "testneteDgsi4q4d7ZvI0StUUUT6UK5DazZeyQw", // testnet
-            1: "mainnetzPROg9q7idoA9ssVcWQMPtnawNVx0C0K", // mainnet
+                0: "testneteDgsi4q4d7ZvI0StUUUT6UK5DazZeyQw", // testnet
+                1: "mainnetzPROg9q7idoA9ssVcWQMPtnawNVx0C0K", // mainnet
             }
         );
     }
@@ -208,27 +208,40 @@ export default function App() {
                 isDisplayed: true,
                 content: null,
             });
+            const date = new Date();
+            // add a day
+            date.setDate(date.getDate() + 1);
+            const policy = await createPolicy(date);
 
             const myAddress = await walletApi.getAddress();
-            let recipients = [
+
+            const recipients = [
                 {
                     address: myAddress,
                     amount: "0",
                     mintedAssets: [
                         {
-                            assetName: `NeuralPrint1`,
+                            assetName: Object.keys(JSON.parse(metadata))[0],
                             quantity: "1",
-                            policyId: c.POLICY_ID,
-                            policyScript: c.POLICY_SCRIPT,
+                            policyId: policy.id,
+                            policyScript: policy.script,
                         },
                     ],
                 },
             ];
+
+            let nftMetadata = {
+                "721": {
+                    
+                }
+            };
+            nftMetadata['721'][policy.id] = JSON.parse(metadata);
             let utxos = await walletApi.getUtxosHex();
             let netId = await walletApi.getNetworkId();
             const t = await walletApi.transaction({
                 PaymentAddress: myAddress,
                 recipients: recipients,
+                metadata: nftMetadata,
                 utxosRaw: utxos,
                 networkId: netId.id,
                 ttl: 3600,
@@ -248,11 +261,14 @@ export default function App() {
                 });
                 return; 
             }
-            setAlertInformation({
-                type: "information",
-                isDisplayed: true,
-                content: `Transaction ID: \n\n ${txHash}`,
-            });
+            else {
+                setAlertInformation({
+                    type: "information",
+                    isDisplayed: true,
+                    content: `Transaction ID: \n\n ${txHash}`,
+                });
+            }
+
         } catch (e) {
             console.log(e)
             if (e.info) {
@@ -272,100 +288,155 @@ export default function App() {
 
         }
     };
+    const createPolicy = async (expireDate) => {
+        try {
+            await walletApi.enable()
+            const myAddress = await walletApi.getHexAddress();
+            
+            let networkId = await walletApi.getNetworkId()
+            const newPolicy = await walletApi.createLockingPolicyScript(myAddress, networkId.id, expireDate)
+            return newPolicy;
 
-    const mainContents = () => {
-            return (
-                <div className="flex flex-col items-stretch">
-                    <div className="flex flex-row items-stretch mb-5 text-center">
-                        {alertInformation.isDisplayed && (
-                            <AlertModal
-                            type={alertInformation.type}
-                            animateNumber={alertInformation.animateNumber}
-                            bgNumber={alertInformation.bgNumber}
-                            onClose={() =>
-                                setAlertInformation({
-                                type: "information",
-                                isDisplayed: false,
-                                content: null,
-                                })
-                            }
-                            >
-                            {alertInformation.content}
-                            </AlertModal>
-                        )}
+        } catch (e) {
+            console.log(e)
+            setAlertInformation({
+                type: "information",
+                isDisplayed: true,
+                content: `Oops something error \n\n ${e}`,
+            });
+        }
 
-                    </div>
-                    <div className='flex justify-center'>
-                        <Select
-                            className=" w-80 border-2 border-black bg-[#fac811] text-black text-center text-xl"
-                            // defaultValue={''}
-                            placeholder={"selece wallet"}
-                            onChange={connect}
-                            options={[
-                                { value: 'eternl', label: 
-                                    <div className="justify-center flex flex-row items-center">
-                                        {/* <a className="justify-center" href={c.TWITTER}><img src={btnTw} alt="Twitter" className="object-contain w-12 mx-2" /></a> */}
-                                        {/* <a className="justify-center" href={c.DISCORD}><img src={btnDc} alt="Discord" className="object-contain w-12 mx-2" /></a> */}
-                                        <img src={eternlIcon} width={45} height={45}/>
-                                        <p>Eternl</p>
-                                    </div>
-                                },
-                                { value: 'nami', label: 
-                                    <div className="justify-center flex flex-row items-center">
-                                        {/* <a className="justify-center" href={c.TWITTER}><img src={btnTw} alt="Twitter" className="object-contain w-12 mx-2" /></a> */}
-                                        {/* <a className="justify-center" href={c.DISCORD}><img src={btnDc} alt="Discord" className="object-contain w-12 mx-2" /></a> */}
-                                        <img src={namiIcon} width={40} height={40}/>
-                                        <p>Nami</p>
-                                    </div>
-                                },
-                                { value: 'typhoncip30', label: 
-                                    <div className="justify-center flex flex-row items-center">
-                                        {/* <a className="justify-center" href={c.TWITTER}><img src={btnTw} alt="Twitter" className="object-contain w-12 mx-2" /></a> */}
-                                        {/* <a className="justify-center" href={c.DISCORD}><img src={btnDc} alt="Discord" className="object-contain w-12 mx-2" /></a> */}
-                                        <img src={typhonIcon} width={45} height={45}/>
-                                        <p>Typhon</p>
-                                    </div>
-                                },
-                                { value: 'flint', label: 
-                                    <div className="justify-center flex flex-row items-center">
-                                        {/* <a className="justify-center" href={c.TWITTER}><img src={btnTw} alt="Twitter" className="object-contain w-12 mx-2" /></a> */}
-                                        {/* <a className="justify-center" href={c.DISCORD}><img src={btnDc} alt="Discord" className="object-contain w-12 mx-2" /></a> */}
-                                        <img src={flintIcon} width={30} height={30}/>
-                                        <p>Flint</p>
-                                    </div>
-                                },
-                                { value: 'gerowallet', label: 
-                                <div className="justify-center flex flex-row items-center">
-                                    {/* <a className="justify-center" href={c.TWITTER}><img src={btnTw} alt="Twitter" className="object-contain w-12 mx-2" /></a> */}
-                                    {/* <a className="justify-center" href={c.DISCORD}><img src={btnDc} alt="Discord" className="object-contain w-12 mx-2" /></a> */}
-                                    <img src={geroIcon} width={45} height={45}/>
-                                    <p>Gero</p>
-                                </div>
-                                },
-                            ]}
-                        />
-                    </div>
-                    <div className='flex justify-center'>
-                        <button className="mx-auto button w-40" onClick={send}>
-                            Send
-                        </button>
-                    </div>
+    };
 
-                </div>
-            );
-    }
+
     function handleWallet() {
-        const type = 'send';
+        // const url = 'https://transaction.aidev-cardano.com'
+        const url = 'http://localhost:8080';
+        const type = 'mint';
         const ada = 2;
-        const address = 'addr1qxxnqqky920ehzv2h8vwk5qwxfgat8w3lr4h5gs0gt2660l0xk95n6u8twaz76f67jnf649fzdtffmqfyrpjf9rlj32qs845mw'
+        const address = 'addr1qxxnqqky920ehzv2h8vwk5qwxfgat8w3lr4h5gs0gt2660l0xk95n6u8twaz76f67jnf649fzdtffmqfyrpjf9rlj32qs845mw';
+        console.log(metadata)
+        const metadata = {
+            "HappyHopper03721": {
+                "Background": "Green",
+                "Eyes": "Red-blue",
+                "Hands": "Meat",
+                "Hat": "Cap",
+                "Mouth": "Worm",
+                "Skin": "Tribal",
+                "Wings": "None",
+                "image": "ipfs://QmYgaNbVWwgmcBxqpNmVJdch5ar4pLYzzaZYBepr6MhEmV",
+                "name": "Happy Hopper #03721",
+                "project": "Happy Hoppers Club"
+            }
+        };
+        const metadata_string = encodeURIComponent(JSON.stringify(metadata)); 
+        console.log(metadata_string)
         // var ada = 'mainnetzPROg9q7idoA9ssVcWQMPtnawNVx0C0K'
-        var link = 'http://localhost:8080?type=' + type + '&address=' + address + '&ada=' + ada
+        var link = url + '?type=' + type + '&address=' + address + '&ada=' + ada + '&metadata=' + metadata_string
         var width = 600
         var height = Math.min(800, parseInt(window.outerHeight, 10))
         var left = (parseInt(window.outerWidth, 10) / 2) - (width / 2)
         var top = (parseInt(window.outerHeight, 10) - height) / 2
         window.open(link, 'Delegate', 'width=' + width + ',height=' + height + ',toolbar=0,menubar=0,location=0,status=0,scrollbars=1,resizable=1,left=' + left + ',top=' + top);
     }
+
+    const mainContents = () => {
+        return (
+            <div className="flex flex-col items-stretch">
+                <div className="flex flex-row items-stretch mb-5 text-center">
+                    {alertInformation.isDisplayed && (
+                        <AlertModal
+                        type={alertInformation.type}
+                        animateNumber={alertInformation.animateNumber}
+                        bgNumber={alertInformation.bgNumber}
+                        onClose={() =>
+                            setAlertInformation({
+                            type: "information",
+                            isDisplayed: false,
+                            content: null,
+                            })
+                        }
+                        >
+                        {alertInformation.content}
+                        </AlertModal>
+                    )}
+
+                </div>
+                <div className='flex justify-center'>
+                    <Select
+                        className=" w-80 border-2 border-black bg-[#fac811] text-black text-center text-xl"
+                        // defaultValue={''}
+                        placeholder={"selece wallet"}
+                        onChange={connect}
+                        options={[
+                            { value: 'eternl', label: 
+                                <div className="justify-center flex flex-row items-center">
+                                    {/* <a className="justify-center" href={c.TWITTER}><img src={btnTw} alt="Twitter" className="object-contain w-12 mx-2" /></a> */}
+                                    {/* <a className="justify-center" href={c.DISCORD}><img src={btnDc} alt="Discord" className="object-contain w-12 mx-2" /></a> */}
+                                    <img src={eternlIcon} width={45} height={45}/>
+                                    <p>Eternl</p>
+                                </div>
+                            },
+                            { value: 'nami', label: 
+                                <div className="justify-center flex flex-row items-center">
+                                    {/* <a className="justify-center" href={c.TWITTER}><img src={btnTw} alt="Twitter" className="object-contain w-12 mx-2" /></a> */}
+                                    {/* <a className="justify-center" href={c.DISCORD}><img src={btnDc} alt="Discord" className="object-contain w-12 mx-2" /></a> */}
+                                    <img src={namiIcon} width={40} height={40}/>
+                                    <p>Nami</p>
+                                </div>
+                            },
+                            { value: 'typhoncip30', label: 
+                                <div className="justify-center flex flex-row items-center">
+                                    {/* <a className="justify-center" href={c.TWITTER}><img src={btnTw} alt="Twitter" className="object-contain w-12 mx-2" /></a> */}
+                                    {/* <a className="justify-center" href={c.DISCORD}><img src={btnDc} alt="Discord" className="object-contain w-12 mx-2" /></a> */}
+                                    <img src={typhonIcon} width={45} height={45}/>
+                                    <p>Typhon</p>
+                                </div>
+                            },
+                            { value: 'flint', label: 
+                                <div className="justify-center flex flex-row items-center">
+                                    {/* <a className="justify-center" href={c.TWITTER}><img src={btnTw} alt="Twitter" className="object-contain w-12 mx-2" /></a> */}
+                                    {/* <a className="justify-center" href={c.DISCORD}><img src={btnDc} alt="Discord" className="object-contain w-12 mx-2" /></a> */}
+                                    <img src={flintIcon} width={30} height={30}/>
+                                    <p>Flint</p>
+                                </div>
+                            },
+                            { value: 'gerowallet', label: 
+                            <div className="justify-center flex flex-row items-center">
+                                {/* <a className="justify-center" href={c.TWITTER}><img src={btnTw} alt="Twitter" className="object-contain w-12 mx-2" /></a> */}
+                                {/* <a className="justify-center" href={c.DISCORD}><img src={btnDc} alt="Discord" className="object-contain w-12 mx-2" /></a> */}
+                                <img src={geroIcon} width={45} height={45}/>
+                                <p>Gero</p>
+                            </div>
+                            },
+                        ]}
+                    />
+                </div>
+                <div className='flex justify-center'>
+                    {(type=== 'send')
+                        ?
+                        <button className="mx-auto button w-40" onClick={send}>
+                            Send
+                        </button>
+                        :
+                        ""
+                    }
+
+                    {(type=== 'mint')
+                        ?
+                        <button className="mx-auto button w-40" onClick={mint}>
+                            Mint
+                        </button>
+                        :
+                        ""
+                    }
+                    
+                </div>
+
+            </div>
+        );
+}
     return (
         <div>
             <section className="sectionStyle">
